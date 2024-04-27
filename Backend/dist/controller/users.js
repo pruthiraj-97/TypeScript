f2f9ef786b0f84aece58ad68cd829d7618267282
pruthiraj-97 <b122085@iiit-bh.ac.in>
2024-04-28 00:42:08 +0530

    add code

--- Backend/dist/controller/users.js
+++ Backend/dist/controller/users.js
@@ -5,7 +5,16 @@ function getUser(req, res) {
     return res.send("user");
 }
 exports.getUser = getUser;
+// request type can be set by generic          body query                                       Response type
 function getUserById(req, res) {
-    return res.send("user/id");
+    // console.log(req.body.email,req.body.password,req.body.username)
+    // console.log(req.query.loginAfterCreate)
+    //console.log(req.params.id)
+    console.log(req.customfield);
+    return res.send({
+        id: "dd mherbfer",
+        email: "nvjrehr",
+        username: "ldjnvrjeh"
+    });
 }
 exports.getUserById = getUserById;
