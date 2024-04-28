6bf0b330f283f1a3b434c1e0e540fc8d2be5539a
pruthiraj-97 <b122085@iiit-bh.ac.in>
2024-04-28 09:37:54 +0530

    expanding request

--- Backend/src/controller/users.ts
+++ Backend/src/controller/users.ts
@@ -6,7 +6,7 @@ import session from "express-session";
 export function getUser(req:Request,res:Response){
    return res.send("user")
 }
-// request type can be set by generic          body query                                       Response type
+// request type can be set by generic                            body query                                       Response type
 export function getUserById(req:Request<{id:string},{},CreateUserDto,userqueryparams>,res:Response<User>){
     // console.log(req.body.email,req.body.password,req.body.username)
     // console.log(req.query.loginAfterCreate)
